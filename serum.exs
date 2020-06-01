e432a121fb6e16363480020cb4d0caf1f40f2555
Tonny Adhi Sabastian <tonny.adhi@ruma.co.id>
2020-06-01 10:09:44 +0700

    Update sitemap

--- serum.exs
+++ serum.exs
@@ -1,6 +1,7 @@
 %{
   site_name: "Segmentation Fault",
   site_description: "Segfault is better than Kernel Panic",
+  server_root: "https://segmentationfault.xyz",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
   author: "Tonny Adhi Sabastian",
